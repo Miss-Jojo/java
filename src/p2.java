593432f342c3259b1964a48e677c15a912093335
Miss-Jojo <debasmitasarkar1207@gmail.com>
2025-12-04 17:47:30 +0530

    .

---
+++ src/p2.java
@@ -0,0 +1,13 @@
+class Details {
+    public static void main(String[] args) {
+        String name = "Debasmita Srakar";
+        String roll = "2405425";
+        String section = "cse-7";
+        String branch = "cse btech";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + roll);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+}
